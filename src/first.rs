def281b3c2d970c04de308cb8188ee6ad47bdf84
JohnLee <lijiang01001@163.com>
2024-01-01 23:49:58 +0800

    List Node

--- src/first.rs
+++ src/first.rs
@@ -1,4 +1,13 @@
-pub enum List{
-    None,
-    Elem(i32, Box<List>)
+pub struct List{
+    head : Link
+}
+
+enum Link{
+    Empty,
+    More(Box<Node>)
+}
+
+struct Node{
+    elem : i32,
+    next : Link
 }
