53814e9990c9259dd96365231a3e4da97e3adff2
JohnLee <lijiang01001@163.com>
2024-01-02 20:42:21 +0800

    栈链表

--- src/first.rs
+++ src/first.rs
@@ -1,3 +1,5 @@
+use std::mem;
+
 pub struct List{
     head : Link
 }
@@ -10,4 +12,67 @@ enum Link{
 struct Node{
     elem : i32,
     next : Link
+}
+
+impl List{
+    pub fn new() -> List {
+        List { head: Link::Empty }
+    }
+
+    pub fn push(&mut self, val : i32){
+        let new_node = Node{
+            elem : val,
+            next : mem::replace(&mut self.head, Link::Empty)
+        };
+        self.head = Link::More(Box::new(new_node))
+    }
+
+    pub fn pop(&mut self) -> Option<i32> {
+        match mem::replace(&mut self.head, Link::Empty){
+            Link::Empty => {
+                None
+            },
+            Link::More(node) => {
+                self.head = node.next;
+                Some(node.elem)
+            }
+        }
+    }
+}
+
+impl Drop for List{
+    fn drop(&mut self) {
+        let mut link = mem::replace(&mut self.head, Link::Empty);
+        while let Link::More(mut node) = link{
+            link = mem::replace(&mut node.next, Link::Empty);
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::List;
+
+    #[test]
+    fn list_test() {
+        let mut list = List::new();
+
+        assert_eq!(list.pop(), None);
+
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(2));
+
+        list.push(4);
+        list.push(5);
+
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), Some(4));
+
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), None);
+    }
 }
